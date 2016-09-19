67f87d8f8d6f5033a4186b9f8ac1205402e80108
Philippe Mathieu <philippe.mathieu@idilia.com>
2016-09-19 10:53:30 -0400

    Idilia theme

--- src/themes/idl/index.js
+++ src/themes/idl/index.js
@@ -1,8 +1,10 @@
 module.exports = [
   '<div class="trip-block">',
+   '<div class="trip-top">',
    '<a href="#" class="trip-close"></a>',
-   '<div class="progress progress-bar-default"><div id="trip-progress" style="width: 0%;opacity:0.4;" aria-valuemax="100" aria-valuemin="0" aria-valuenow="43" role="progressbar" class="progress-bar"></div></div>',
    '<div class="trip-header"></div>',
+   '<div class="progress progress-bar-default"><div id="trip-progress" style="width: 0%;opacity:0.4;" aria-valuemax="100" aria-valuemin="0" aria-valuenow="43" role="progressbar" class="progress-bar"></div></div>',
+   '</div>',
    '<div class="trip-content"></div>',
    '<div class="trip-progress-steps"></div>',
    '<div class="trip-navigation">',
